031622cd2bae38b3acf42412a6244e8f250283ea
Kaila <kailaamia@gmail.com>
2024-01-16 21:32:56 -0600

    Added dependencies

---
+++ index.js
@@ -0,0 +1,15 @@
+//Import dependencies
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+//Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    },
+    console.log(`Connected to the company_db database.`)
+);
