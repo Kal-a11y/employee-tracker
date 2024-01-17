18421fe0043a2cfa313aef5a5e864ef883c73f1a
Kaila <kailaamia@gmail.com>
2024-01-17 16:23:15 -0600

    Added prompts

--- index.js
+++ index.js
@@ -13,3 +13,103 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the company_db database.`)
 );
+
+const mainPrompt = [
+    {
+        type: 'list',
+        message: 'what would you like to do?',
+        name: 'mainTodo',
+        choices: [
+            'View all departments',
+            'Add a department',
+            'View all roles',
+            'Add a role',
+            'View all employees',
+            'Add an employee',
+            'Update an employee role',
+            'Quit'
+        ]
+    },
+];
+
+const addDepartmentPrompt = [
+    {
+        type: 'input',
+        message: 'What is the name of the department?',
+        name: 'departmentName',
+    },
+];
+const addRolePrompt = [
+    {
+        type: 'input',
+        message: 'What is the name of the role?',
+        name: 'roleName',
+    },
+    {
+        type: 'input',
+        message: 'What is the salary of the role?',
+        name: 'roleSalary',
+    },
+    {
+        type: 'list',
+        message: 'Which department does the role belong to?',
+        name: 'departmentList',
+        choices: [
+            //call array of departments dynamiclly
+        ]
+    },
+];
+const addEmployeePrompt = [
+    {
+        type: 'input',
+        message: 'What is the first name of the employee?',
+        name: 'employeeFirstName',
+    },
+    {
+        type: 'input',
+        message: 'What is the last name of the employee?',
+        name: 'employeeLastName',
+    },
+    {
+        type: 'list',
+        message: "What is the employee's role?",
+        name: 'roleList',
+        choices: [
+            //call array of roles dynamiclly
+            'Example option 1',
+            'Example option 2',
+        ]
+    },
+    {
+        type: 'list',
+        message: "who is the employee's manager?",
+        name: 'roleList',
+        choices: [
+            //call array of roles dynamiclly
+            'Example option 1',
+            'None',
+        ]
+    },
+]
+const updateEmployeePrompt = [
+    {
+        type: 'list',
+        message: "Which employee's role do you want to update?",
+        name: 'employeeList',
+        choices: [
+            //call array of employees dynamiclly
+            'Example option 1',
+            'None',
+        ]
+    },
+    {
+        type: 'list',
+        message: "Which role do you want to assign the seledcted employee?",
+        name: 'roleList',
+        choices: [
+            //call array of roles dynamiclly
+            'Example option 1',
+            'None',
+        ]
+    },
+]
